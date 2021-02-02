81ac206246026fd6e996a56d787604c8f2698ce4
ximenking <hangm@lotuseed.com>
2021-02-02 10:54:46 +0800

    add RandomInPlace

---
+++ introduction_to_algorithms/chapter5/RandomInPlace/test_class/test_class.h
@@ -0,0 +1,15 @@
+#ifndef TEST_CLASS_H
+#define TEST_CLASS_H
+
+#include <vector>
+
+class solution{
+public:
+    void RandomInPlace(std::vector<int> &v);
+
+private:
+
+};
+
+
+#endif // TEST_CLASS_H
