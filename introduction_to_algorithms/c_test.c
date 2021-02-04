99157b03a49eed6483f1483099b4414bdcd9a2c8
ximenking <hangm@lotuseed.com>
2021-02-04 17:17:28 +0800

    add priority queue

--- introduction_to_algorithms/c_test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-    int a;
-    a ^= a;
-    printf("%d\n",a);
-
-    return 0;
-}
---
+++ introduction_to_algorithms/chapter6/priority_queue/main.cpp
@@ -0,0 +1,8 @@
+#include "priority_queue/priority_queue.h"
+
+int main(void){
+    TestPQ tpq;
+    tpq.Test();
+
+    return 0;
+}
