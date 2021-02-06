8f34942429ed71696cf16b883ab270c2388a85f0
hanguanmiao <diaodabijin@gmail.com>
2021-02-06 19:26:25 +0800

    add CountingSort

---
+++ introduction_to_algorithms/chapter8/CountingSort/test_class/test_class.h
@@ -0,0 +1,18 @@
+#ifndef TEST_CLASS_H
+#define TEST_CLASS_H
+
+#include <vector>
+
+class solution{
+public:
+    std::vector<int> CountingSort(std::vector<int> &vec, int minval, int maxval);
+private:
+
+};
+
+class Test{
+public:
+    void TestCS(void);
+};
+
+#endif // TEST_CLASS_H
