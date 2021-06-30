fd82b54a2d2befde3ca9f7fed9faaca0eb4330d3
hacker <diaodabijin@gmail.com>
2021-06-30 22:03:51 +0800

    modify TargetSum.cpp

--- leetcode/problems/494/TargetSum.cpp
+++ leetcode/problems/494/TargetSum.cpp
@@ -7,23 +7,25 @@ using namespace std;
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int sum = 0;
-        for (int& num : nums) {
-            sum += num;
-        }
-        int diff = sum - target;
-        if (diff < 0 || diff % 2 != 0) {
-            return 0;
-        }
-        int neg = diff / 2;
-        vector<int> dp(neg + 1);
-        dp[0] = 1;
-        for (int& num : nums) {
-            for (int j = neg; j >= num; j--) {
-                dp[j] += dp[j - num];
+        int count = 0;
+        findTargetSumWaysInternal(nums, 0, target, 0, count);
+        return count;
+    }
+private:
+    void findTargetSumWaysInternal(vector<int>& nums, int idx, int target, int sum, int &count){
+        int sum1 = sum + nums[idx];
+        int sum2 = sum - nums[idx++];
+        if(idx == static_cast<int>(nums.size())){
+            if(sum1 == target){
+                ++count;
             }
+            if(sum2 == target){
+                ++count;
+            }
+        }else{
+            findTargetSumWaysInternal(nums, idx, target, sum1, count);
+            findTargetSumWaysInternal(nums, idx, target, sum2, count);
         }
-        return dp[neg];
     }
 };
 
@@ -32,8 +34,20 @@ int main(void){
     vector<int> input;
     Solution s;
 
+    input = {1};
+    cout << s.findTargetSumWays(input, 1) << endl; // 1
+
+    input = {1};
+    cout << s.findTargetSumWays(input, 2) << endl; // 0
+
+    input = {1,1,1,1,1};
+    cout << s.findTargetSumWays(input, 3) << endl; // 5
+
+    input = {1,1,1,1,1,1};
+    cout << s.findTargetSumWays(input, 3) << endl; // 0
+
     input = {1,2,3,4,5,6,7};
-    cout << s.findTargetSumWays(input, 12) << endl;
+    cout << s.findTargetSumWays(input, 12) << endl; // 5
 
     return 0;
 }
