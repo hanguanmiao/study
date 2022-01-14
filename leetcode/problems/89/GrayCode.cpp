877220f8c2263abc18aa858c67b15fc58f19e02c
hacker <diaodabijin@gmail.com>
2022-01-14 22:39:52 +0800

    modify GrayCode.cpp

--- leetcode/problems/89/GrayCode.cpp
+++ leetcode/problems/89/GrayCode.cpp
@@ -2,24 +2,38 @@
 
 
 
+//class Solution {
+//public:
+//    std::vector<int> grayCode(int n) {
+//        std::vector<int> res;
+
+//        res.push_back(0);
+
+//        for(int i=0; i<n; ++i){
+//            int size = static_cast<int>(res.size());
+
+//            for(int j=size-1; j>=0; --j){
+//                res.push_back(res[j] | (1 << i));
+//            }
+//        }
+
+//        return res;
+//    }
+//};
+
 class Solution {
 public:
     std::vector<int> grayCode(int n) {
         std::vector<int> res;
 
-        res.push_back(0);
-
-        for(int i=0; i<n; ++i){
-            int size = static_cast<int>(res.size());
-
-            for(int j=size-1; j>=0; --j){
-                res.push_back(res[j] | (1 << i));
-            }
+        for(int i=0; i<(1<<n);++i){
+            res.push_back(i^(i>>1));
         }
 
         return res;
     }
 };
+
 
 
 void test(void);
